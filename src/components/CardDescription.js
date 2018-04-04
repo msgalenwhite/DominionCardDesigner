652e2ae75dd81db16443922c2f587fac921fe3c3
Galen <galen.h.white@gmail.com>
2018-04-04 16:18:09 -0400

    Fixed issue where 'cardDraws' was missing an 's'

--- src/components/CardDescription.js
+++ src/components/CardDescription.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const CardDescription = props => {
+/*
+GOALS:
+  1. If the number is 0, then don't put a label
+  2. If the number is 1 || -1, then don't put a "s"
+  3. If the number is anything else, put the whole word with an "s"
+*/
 
-
+debugger
   return(
     <div className={props.divTag}>
       <p className='descriptionPiece'>+{props.actions} Actions</p>
