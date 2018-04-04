15dd9aa3e49c281612a814396a86b968d251b75c
Galen <galen.h.white@gmail.com>
2018-04-04 16:44:17 -0400

    Card description correctly displays Actions, Cards and Buys

--- src/components/CardDescription.js
+++ src/components/CardDescription.js
@@ -8,12 +8,41 @@ GOALS:
   3. If the number is anything else, put the whole word with an "s"
 */
 
-debugger
+// SAMPLE PROPS:
+// {divTag: "cardTextDiv actioncardTextDiv", actions: 1, draws: 2, buys: 3, text: "This is a test"}
+
+  const labels = [
+    {
+      value: props.actions,
+      label: "Action"
+    },
+    {
+      value: props.draws,
+      label: "Card"
+    },
+    {
+      value: props.buys,
+      label: "Buy"
+    }
+  ]
+
+  let descriptionComponents = labels.map((infoObject) => {
+    let number = infoObject.value
+
+    if (number === 1 || number === -1) {
+      return (
+        <p className='descriptionPiece'>+{number} {infoObject.label}</p>
+      )
+    } else if (number !== 0) {
+      return (
+        <p className='descriptionPiece'>+{number} {infoObject.label}s</p>
+      )
+    }
+  })
+
   return(
     <div className={props.divTag}>
-      <p className='descriptionPiece'>+{props.actions} Actions</p>
-      <p className='descriptionPiece'>+{props.draws} Cards</p>
-      <p className='descriptionPiece'>+{props.buys} Buys</p>
+      {descriptionComponents}
       <p className='descriptionPiece'>{props.text}</p>
     </div>
   )
